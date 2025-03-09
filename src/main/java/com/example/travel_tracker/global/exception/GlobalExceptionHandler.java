4a43af4f88e0419b6666d3f118a00fd4c18a0d42
jaea-kim <jaeakim98@gmail.com>
2025-03-09 16:42:37 +0900

    feat: 만들어둔 기본 exception을 처리하는 핸들러 생성 #2
    - 커스텀 exception을 만들 때 기본 exception을 상속해서 만들면 해당 핸들러가 동작하도록 함

---
+++ src/main/java/com/example/travel_tracker/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.travel_tracker.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(BaseException.class)
+	public ResponseEntity<ErrorResponse> handleBaseException(BaseException exception) {
+		ErrorResponse errorResponse = ErrorResponse.of(exception.getStatus(), exception.getErrorCode(),
+			exception.getMessage(), exception.getClass().getName());
+
+		return ResponseEntity.status(exception.getStatus()).body(errorResponse);
+	}
+}
