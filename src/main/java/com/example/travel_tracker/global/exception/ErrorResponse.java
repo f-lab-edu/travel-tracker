10f3f604ec24166420d17b7443fff9568487188a
jaea-kim <jaeakim98@gmail.com>
2025-03-09 16:41:11 +0900

    feat: 공통 에러 응답 생성 #2

---
+++ src/main/java/com/example/travel_tracker/global/exception/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.example.travel_tracker.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ErrorResponse {
+	private final HttpStatus status;
+	private final String errorCode;
+	private final String message;
+	private final String exceptionType;
+
+	public static ErrorResponse of(HttpStatus status, String errorCode, String message, String exceptionType) {
+		return new ErrorResponse(status, errorCode, message, exceptionType);
+	}
+}
