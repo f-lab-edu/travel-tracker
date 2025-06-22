0a01928d69b34188b262e2019eaac37efe0eb3da
jaea-kim <jaeakim98@gmail.com>
2025-06-22 11:47:31 +0900

    feat: User 엔티티 작성 #5

---
+++ src/main/java/com/project/triplog/domain/User.java
@@ -0,0 +1,42 @@
+package com.project.triplog.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.project.triplog.dto.JoinRequest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String name;
+	private String email;
+	private String password;
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	public static User from(JoinRequest joinRequest) {
+		User user = new User();
+		user.username = joinRequest.getUsername();
+		user.name = joinRequest.getName();
+		user.email = joinRequest.getEmail();
+		user.password = joinRequest.getPassword();
+		return user;
+	}
+}
