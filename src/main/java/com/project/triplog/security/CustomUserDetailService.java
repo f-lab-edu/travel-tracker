b00882cb14c939e9175e2543c0b91c1028819313
jaea-kim <jaeakim98@gmail.com>
2025-08-03 18:28:03 +0900

    feat: @AuthenticationPrincipal에서 유저 정보를 받기 위해 설정 #6
    - UserDetailService와 UserDetail 인터페이스를 확장해서 구현

---
+++ src/main/java/com/project/triplog/security/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package com.project.triplog.security;
+
+import com.project.triplog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserPrincipal::new)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
+    }
+}
---
+++ src/main/java/com/project/triplog/security/UserPrincipal.java
@@ -0,0 +1,31 @@
+package com.project.triplog.security;
+
+import com.project.triplog.domain.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
