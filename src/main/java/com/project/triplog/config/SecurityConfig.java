7fb7be91802892b424ecd59b3bec46864a5aa17c
jaea-kim <jaeakim98@gmail.com>
2025-08-03 18:38:28 +0900

    feat: username에서 UserDetails 기반으로 개선 #6

--- src/main/java/com/project/triplog/config/SecurityConfig.java
+++ src/main/java/com/project/triplog/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.project.triplog.config;
 
+import com.project.triplog.security.CustomUserDetailService;
+import com.project.triplog.security.JwtAuthenticationFilter;
+import com.project.triplog.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,28 +13,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.project.triplog.security.JwtAuthenticationFilter;
-import com.project.triplog.security.JwtTokenProvider;
-
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-	private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailService userDetailService;
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
-			auth -> auth.requestMatchers(
-					"/users/join", "/users/validation", "/users/email/verification", "/users/login")
-				.permitAll().anyRequest().authenticated()
-		).addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-		return httpSecurity.build();
-	}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider, userDetailService);
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+                auth -> auth.requestMatchers(
+                                "/users/join", "/users/validation", "/users/email/verification", "/users/login")
+                        .permitAll().anyRequest().authenticated()
+        ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
